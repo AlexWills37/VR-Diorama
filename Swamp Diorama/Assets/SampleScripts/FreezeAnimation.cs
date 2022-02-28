7bb55805f11af4b1c7fd8ff3d78d9ac8b81f02df
AlexWills37 <alexanderwills37@gmail.com>
2022-02-28 16:02:46 -0500

    Animated Shrek

---
+++ Swamp Diorama/Assets/SampleScripts/FreezeAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator myAnimationController;
+
+    void Start()
+    {
+        
+    }
+
+    //Start the animation when hover starts
+    public void unfreezeAnimation()
+    {
+        myAnimationController.speed = 1;
+    }
+
+    //Stop the animation when hover ends
+    public void freezeAnimation()
+    {
+        myAnimationController.speed = 0;
+    }
+}
